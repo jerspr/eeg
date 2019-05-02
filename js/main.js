aeff303e5a3c1d910c7636b6e003c1f849c0a141
Jesser Rodrigo Pariona huarcaya <jerspr@MacBook-Pro-de-Jesser.local>
2019-05-02 15:26:43 -0500

    update

--- js/main.js
+++ js/main.js
@@ -2,10 +2,33 @@ var obj = {
 	init: function(){
 		obj.stickitHeader();
 		obj.searchExpand();
+        obj.navMenu();
 	},
 	stickitHeader: function(){
 		$('.nav').stickit({extraHeight: 0});
 	},
+    navMenu:function(){
+        function e(){
+            document.getElementById("mySidenav").style.height="280px"
+        }
+        function o(){
+            document.getElementById("mySidenav").style.height="0"
+        }
+        $("#openNav").click(function(o){
+            o.preventDefault(),
+            //$(".header").removeClass("p-menu-nav"),
+            $("#mySidenav").addClass("active-menu"),
+            $("body").addClass("overflowBody"),
+            e()
+        }),
+        $("#closeNav").click(function(e){
+            e.preventDefault(),
+            $("#mySidenav").removeClass("active-menu"),
+            $("body").removeClass("overflowBody"),
+            //$("body").hasClass("p-bloque")&&($(".header").hasClass("stick")||$(".header").addClass("p-menu-nav")),
+            o()
+        })
+    },
 	searchExpand: function(){
 		function buttonUp(){
             var inputVal = $('.searchbox-input').val();
